cb6fc85378223b00ffcf49785bcfaaf0d6622830
Jehoon <limjhoon8@gmail.com>
2024-03-19 12:47:12 +0900

    #0319

---
+++ 1. core javascript/1. 자바스크립트 기초/4. 비교연산자.js
@@ -0,0 +1,26 @@
+
+var a = 5;
+var b = '5';
+
+console.log(a === b);
+console.log(a !== b);   // 자바에서는 !일때도 = 2개 , ==도 ===세개로
+
+console.log('=========================');
+
+
+// == 비교는 결과 예측이 어려움  === 로 하면 됨.
+console.log('0' == '')
+console.log(0 == '')
+console.log('0' == 0)
+console.log(false == 'false')
+console.log(false == '0')
+
+
+// 대소 비교   문자열 대소는 십진법 숫자로.
+console.log('========================');
+console.log('a' > 'A');
+console.log('c' < 'a');
+console.log('강' < '황');
+
+
+console.log('ace' < 'ade');
