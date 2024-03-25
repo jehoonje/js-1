147431d12e1a849c89b8c6ed15d5ee1ef996edfa
Jehoon <limjhoon8@gmail.com>
2024-03-25 10:32:05 +0900

    #0325

--- 1. core javascript/3. 형변환 casting/3. 단축평가.js
+++ 1. core javascript/3. 형변환 casting/3. 단축평가.js
@@ -1,1 +1,21 @@
 
+
+
+
+
+
+if (조건) {
+  alert('blahvblah');
+}
+
+조건 && alert('blah balh~');
+
+/*
+  <h1>안녕하세요</h1>
+  login && <h2>xxx님 환영합니다</h2>
+  
+  쿠폰당첨여부 && sendCoupon();
+  내게시물여부 && <button>삭제</button>
+
+  !쿠폰당첨여부 && sendCoupon();
+*/
