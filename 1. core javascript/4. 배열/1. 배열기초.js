15f952a10968db6057dfbc6e30e7b867930652e8
Jehoon <limjhoon8@gmail.com>
2024-03-25 10:56:20 +0900

    #0325

---
+++ 1. core javascript/4. 배열/1. 배열기초.js
@@ -0,0 +1,53 @@
+
+
+let food1 = '짬뽕';
+let food2 = '탕수육';
+let food3 = '자몽';
+let all = `${food1}, ${food2}, ${food3}`;
+
+
+// 배열 //
+// 변수 하나로 여러 데이터 묶어쓸려고
+// 데이터 찾기쉽고
+// 1열로
+// 배열 === 객체(여러데이터가 있는것) === object
+
+let arr = [10, 20 , 30, 40];
+console.log(typeof arr);
+
+console.log(arr[1]); // 20
+
+console.log(arr[2] ** 2); // 900
+
+
+arr[1]  =  999;
+arr[3]++;
+
+console.log(arr);
+
+// 배열 데이터 수 확인
+console.log(arr.length);
+
+console.log(`첫번째 데이터: ${arr[0]}`);
+console.log(`마지막 데이터: ${arr[arr.length - 1]}`);
+
+// 배열데이터 순회 ( 전체 참조 : travis )
+console.log('========================');
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+
+// for ~ of 반복문 (배열 전용 반복문) 
+let weekDays = ['월','화','수','목','금','토','일'];
+console.log('================================');
+
+for (let i = 0; i < weekDays.length; i++) {
+  console.log(`${weekDays[i]}요일!!`);
+}   // 인덱스 조건걸기 편함 홀수 거르기 라던가
+
+for (let day of weekDays) {
+  console.log(`${day}요일!!`);
+}  // 자동으로 n 바퀴 돌면서 전체순회를 해서 출력함.
+
