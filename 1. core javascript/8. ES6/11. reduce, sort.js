846f5efe915c11af788d392f74df34355e62dbdb
Jehoon <limjhoon8@gmail.com>
2024-03-28 15:25:26 +0900

    #0328

--- 1. core javascript/8. ES6/11. reduce, sort.js
+++ 1. core javascript/8. ES6/11. reduce, sort.js
@@ -65,3 +65,53 @@ const appleBasket = [{
 },
 ];
 
+
+// 배열고차함수 reduce : 배열의 각 요소들을 주어진 콜백에 맞게 합산, 누적하여
+//                       하나의 결과값을 반환하는 함수
+// reduce에 들어가는 callback
+// callback(accumulator, currentValue)
+// accumulator: 어떤 데이터를 계속 쌓아가는 변수
+// currentValue: 현재 루프회차에서 사용할 데이터
+// reduce는 반복을 실행할때마다 currentValue를 accumulator에 누적함
+
+
+
+const result = [10, 20, 30, 40, 50, 60].reduce((a, b) => {
+  // console.log(a);
+  // console.log(b);
+  // console.log(`${a} + ${b} = ${a + b}`);
+  // console.log('=============');
+  return a + b;
+});
+
+// reduce의 콜백함수 다음파라미터는 initialValue를 의미하며 
+// 반복문의 accumulator시작값을 의미함
+// 따라서 0을 시작값으로 두고 return a + b; 를 하면
+// 0 + 1 + 2 + 3 + 4 + 5 + 6
+// 시작값을 없애면 accumulator의 시작값이 0번인덱스로 지정되며
+// 1 + 2 + 3 + 4 + 5 + 6
+
+// console.log(`result: ${result}`);
+
+
+
+// 사과바구니에 있는 사과들의 모든 당도를 합산한 결과 얻기
+
+const resultTotalSweet = appleBasket
+  .reduce((totalSweet, apple) => {
+    console.log(totalSweet);
+    console.log(apple);
+    console.log('============');
+    return totalSweet + apple.sweet;
+  }, 0)
+
+console.log(`사과 당도 총합: ${resultTotalSweet}`);
+
+function myReduce(callback, initialValue) {
+  let accumulator = initialValue === undefined ? array[0] : initialValue;
+  const startIdx = initialvalue === undefined ? 1 : 0;
+  for (let i = startIdx; i < array.length; i++) {
+    accumulator = callback(accumulator, array[i]);
+  }
+  return accumulator;
+}
