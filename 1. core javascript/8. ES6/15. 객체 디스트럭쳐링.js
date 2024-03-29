aebffde7f95692778e0a5dff680dd02fd1278c69
Jehoon <limjhoon8@gmail.com>
2024-03-29 11:36:22 +0900

    #0329

--- 1. core javascript/8. ES6/15. 객체 디스트럭쳐링.js
+++ 1. core javascript/8. ES6/15. 객체 디스트럭쳐링.js
@@ -16,3 +16,45 @@ function foo(employee, age) {
   console.log(`나이는 ${employee.age}입니다.`);
 
 }
+const age = 10;
+
+const { age: empAge, birthDate } = employee; 
+console.log(empAge);
+
+const divStyle = {
+  'font-size': '18px', 
+  'background-color': 'red'
+};
+
+const { 'font-size': fontSize, 'background-color': bgColor } = divStyle;
+console.log(fontSize);
+console.log(bgColor);
+
+console.log('================================');
+
+
+const dog = {
+  kind: '말티즈',
+  name: '해피',
+  age: 3,
+  injection: false
+};
+
+const { kind, age: petAge, ...rest} = dog;
+console.log(kind);
+console.log(petAge);
+console.log(rest);
+
+// 객체 안전 복사
+const copyDog = { ...dog };
+copyDog.age = 10;
+
+
+const copyDog2 = {
+  ...dog,
+  age: 20,
+  favorite: ['산책']
+};
+console.log(dog);
+console.log(copyDog);
+console.log(copyDog2);
