edebf5fbdc7468deb15b9f362a8f503349b4bd80
Jehoon <limjhoon8@gmail.com>
2024-04-02 15:24:15 +0900

    #0402

---
+++ 2. web-api/6. 비동기함수/1. 비동기 기초.js
@@ -0,0 +1,23 @@
+
+
+
+// 비동기함수 setTimeout
+// 코드를 순서대로 진행시키는게 아닌 시간차로 진행시킬 수 있게 함
+
+setTimeout(() => {
+  for (let i = 0; i < 3; i++) {
+    console.log(`hello${i}`);
+    
+  }
+  
+}, 200);   // 밀리초: 1 / 1000초
+
+
+setTimeout(() => {
+  for (let i = 0; i < 4; i++) {
+    console.log(`bye${i}`);
+    
+  }
+  
+}, 0);
+
